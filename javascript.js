e0641b439abbba9adf4a6861b63693893d9699f6
MaxFfsd <reversemax620@gmail.com>
2025-04-14 11:56:15 -0300

    Starting JS tests.

---
+++ javascript.js
@@ -0,0 +1,18 @@
+"use strict";
+console.log("hello, world!");
+let name = "John";
+let surname = "appleseed";
+console.log(name);
+console.log(surname);
+
+const pi = 3.14;
+
+console.log(pi);
+console.log((3 + 2) - 76 * (1 + 1));
+console.log((4 + 6 + 9) / 77);
+let a = 10;
+console.log(a);
+a = 5;
+console.log(a);
+console.log(5 % 2);
+alert('3' + 4);
